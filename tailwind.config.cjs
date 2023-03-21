50e310cf38b34f8e7a4ad46f83d31416d1e2dc49
jonkesito <jonkisnow@gmail.com>
2023-03-20 21:31:00 -0400

    Ready!

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "light-gray": "hsl(212, 45%, 89%)",
+      "grayish-blue": "hsl(220, 15%, 55%)",
+      "dark-blue": "hsl(218, 44%, 22%)",
+      "white-white": "hsl(0, 0%, 100%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
